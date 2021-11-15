72292ca673ca5303cffc42b92c10ee727c843173
keckhung <p10361880@pic.net.tw>
2021-11-15 09:04:35 +0800

    gh-pages dropdownlist placeholder

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/2021-f2e-section1/'
-})
+	plugins: [vue()],
+});
